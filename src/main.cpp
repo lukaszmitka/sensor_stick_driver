d4792b0d57607ae7bf7567340b6fdd79ff03f6ff
Łukasz Mitka <luk.mitka@gmail.com>
2016-06-08 15:50:56 +0200

    Added function for initilizing accelerometer.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,18 @@
 //
 #define BOARD RASPBERRY_PI
 #define ADXL345_ADDRESS 0x53
+#define ADXL_REGISTER_PWRCTL 0x2D
+#define ADXL_PWRCTL_MEASURE 0x8
 
 #include <iostream>
 #include <ros/ros.h>
 #include "gnublin.h"
 
 using namespace std;
+
+unsigned char i2c_buffer;
+
+int init_accel(gnublin_i2c *port);
 
 int main(int argc, char **argv) {
    //ros::init(argc, argv, "ahrs_node");
@@ -18,15 +24,9 @@ int main(int argc, char **argv) {
 
    gnublin_i2c i2c;
 
-
-   i2c.setAddress(ADXL345_ADDRESS); // set the address of the slave you want to read/write
-
+   init_accel(&i2c);
 
    unsigned char RxBuf = 0;
-   unsigned char buf = 0x08;
-
-   i2c.send(0x2d, &buf, 1);
-   //i2c.send(0x12, buffer, 2);   //send 2 bytes from buffer to register-address 0x12
 
    for (int i = 0; i < 10; i++) {
       i2c.receive(0x32, &RxBuf, 1);
@@ -42,3 +42,12 @@ int main(int argc, char **argv) {
    }*/
    return 0;
 };
+
+int init_accel(gnublin_i2c *port) {
+   port->setAddress(ADXL345_ADDRESS); // set the address of the slave you want to read/write
+   // example use:
+   //i2c.send(0x12, &buffer, 2);   //send 2 bytes from buffer to register-address 0x12
+   i2c_buffer = ADXL_PWRCTL_MEASURE;
+   port->send(ADXL_REGISTER_PWRCTL, &i2c_buffer, 1);
+}
+
