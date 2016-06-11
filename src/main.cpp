994446abb6e6e01a1fc1a30451d31606dee503bd
Łukasz Mitka <luk.mitka@gmail.com>
2016-06-11 18:28:59 +0200

    Added function for reading magnetometer.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #define ADXL_REGISTER_BW_RATE 0x2C
 #define ADXL_DATA_FORMAT 0x31
 #define ADXL_PWRCTL_MEASURE 0x8
+#define ADXL_SCALE_FACTOR 0.0039f // 3.9 mg (if range is set to +-2g)
 
 // gyro constants
 #define ITG_3200_ADDRESS 0x68 // device address
@@ -24,13 +25,14 @@
 
 // magnetometer constants
 #define HMC5883L_ADDRESS 0x1e // device address
-#define HMC5883L_SENS_REG 0x03 // 6 bytes X_H, X_L, Z_H, Z_L, Y_H, Y_L
 #define HMC5883L_CONF_REG_A 0x00 // configuration register A
 #define HMC5883L_CONF_REG_B 0x01 // configuration register B
-#define HMC5883L_MODE_REG 0x03 // mode register
+#define HMC5883L_MODE_REG 0x02 // mode register
+#define HMC5883L_SENS_REG 0x03 // 6 bytes X_H, X_L, Z_H, Z_L, Y_H, Y_L
 #define HMC5883L_STATUS_REG 0x09 // status register
 #define HMC5883L_CONF_DATA_A 0b01111000 // 8sample average, 75Hz, normal measurement mode
 #define HMC5883L_CONF_DATA_B 0b00100000 // range +-1.3Ga, 0,92mGa/LSb
+#define HMC5883L_SCALE 0.00092f
 #define HMC5883L_MODE_DATA 0b00000000 // continous measurement mode
 
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
@@ -54,9 +56,11 @@ using namespace std;
 unsigned char i2c_buffer;
 unsigned char accel_buffer[6];
 unsigned char gyro_buffer[8];
-int linear_acceleration[3];
+unsigned char magnet_buffer[6];
+float linear_acceleration[3];
 float rotational_acceleration[3];
 float temperature;
+float magnet_orientation[3];
 unsigned int tmp_axis;
 unsigned int lsb, msb;
 
@@ -66,9 +70,11 @@ int init_gyro(gnublin_i2c *port);
 
 int init_magnet(gnublin_i2c *port);
 
-int read_accel(gnublin_i2c *port, int *x_axis, int *y_axis, int *z_axis);
+int read_accel(gnublin_i2c *port, float *x_axis, float *y_axis, float *z_axis);
 
 int read_gyro(gnublin_i2c *port, float *temp, float *x_axis, float *y_axis, float *z_axis);
+
+int read_magnet(gnublin_i2c *port, float *x_axis, float *y_axis, float *z_axis);
 
 int main(int argc, char **argv) {
    //ros::init(argc, argv, "ahrs_node");
@@ -80,13 +86,13 @@ int main(int argc, char **argv) {
    init_accel(&i2c);
    init_gyro(&i2c);
    init_magnet(&i2c);
+   sleep(1);
 
    for (unsigned char i = 0; i < 10; i++) {
       read_accel(&i2c, &linear_acceleration[0], &linear_acceleration[1], &linear_acceleration[2]);
-
-
       read_gyro(&i2c, &temperature, &rotational_acceleration[0], &rotational_acceleration[1],
                 &rotational_acceleration[2]);
+      read_magnet(&i2c, &magnet_orientation[0], &magnet_orientation[1], &magnet_orientation[2]);
       //cout << "Temp: " << temperature << endl;
       //printf("i = %d, byte: "BYTE_TO_BINARY_PATTERN"\n", (signed char)i, BYTE_TO_BINARY(
       //      (unsigned
@@ -125,7 +131,7 @@ int init_accel(gnublin_i2c *port) {
    return 0;
 }
 
-int read_accel(gnublin_i2c *port, int *x_axis, int *y_axis, int *z_axis) {
+int read_accel(gnublin_i2c *port, float *x_axis, float *y_axis, float *z_axis) {
    // set the address of the slave
    if (port->setAddress(ADXL345_ADDRESS) == -1) {
       return -1;
@@ -137,19 +143,19 @@ int read_accel(gnublin_i2c *port, int *x_axis, int *y_axis, int *z_axis) {
    lsb = accel_buffer[0];
    msb = (signed char) accel_buffer[1];
    tmp_axis = lsb + (msb << 8);
-   *x_axis = (signed int) tmp_axis;
+   *x_axis = ((signed int) tmp_axis) * ADXL_SCALE_FACTOR;
 
    // y_axis
    lsb = accel_buffer[2];
    msb = (signed char) accel_buffer[3];
    tmp_axis = lsb + (msb << 8);
-   *y_axis = (signed int) tmp_axis;
+   *y_axis = ((signed int) tmp_axis) * ADXL_SCALE_FACTOR;
 
    // z_axis
    lsb = accel_buffer[4];
    msb = (signed char) accel_buffer[5];
    tmp_axis = lsb + (msb << 8);
-   *z_axis = (signed int) tmp_axis;
+   *z_axis = ((signed int) tmp_axis) * ADXL_SCALE_FACTOR;
 
    //cout << "x: " << *x_axis << ", y: " << *y_axis << ", z: " << *z_axis << endl;
    return 0;
@@ -237,6 +243,10 @@ int init_magnet(gnublin_i2c *port) {
    i2c_buffer = HMC5883L_MODE_DATA;
    if (port->send(HMC5883L_MODE_REG, &i2c_buffer, 1) == -1) {
       return -4;
+   } else if (port->receive(HMC5883L_MODE_REG, &i2c_buffer, 1) == -1) {
+      return -4;
+   } else {
+      printf("Mode: "BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(i2c_buffer));
    }
 
    if (port->receive(HMC5883L_STATUS_REG, &i2c_buffer, 1) == -1) {
@@ -244,6 +254,44 @@ int init_magnet(gnublin_i2c *port) {
    }
    cout << "Magnetometer status: " << i2c_buffer;
    printf("; "BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(i2c_buffer));
+
+   return 0;
+}
+
+int read_magnet(gnublin_i2c *port, float *x_axis, float *y_axis, float *z_axis) {
+   if (port->setAddress(HMC5883L_ADDRESS) == -1) {
+      return -1;
+   }
+   if (port->receive(HMC5883L_SENS_REG, magnet_buffer, 6) == -1) {
+      return -2;
+   }
+
+   // x_axis
+   lsb = magnet_buffer[1];
+   msb = (signed char) magnet_buffer[0];
+   tmp_axis = lsb + (msb << 8);
+   printf("Gyro x: "BYTE_TO_BINARY_PATTERN" ", BYTE_TO_BINARY(magnet_buffer[0]));
+   printf(""BYTE_TO_BINARY_PATTERN" ", BYTE_TO_BINARY(magnet_buffer[1]));
+   *x_axis = ((signed int) tmp_axis) * HMC5883L_SCALE;
+   printf("; %f, ", *x_axis);
+
+   // y_axis
+   lsb = magnet_buffer[5];
+   msb = (signed char) magnet_buffer[4];
+   tmp_axis = lsb + (msb << 8);
+   printf(", y: "BYTE_TO_BINARY_PATTERN" ", BYTE_TO_BINARY(magnet_buffer[4]));
+   printf(""BYTE_TO_BINARY_PATTERN" ", BYTE_TO_BINARY(magnet_buffer[5]));
+   *y_axis = ((signed int) tmp_axis) * HMC5883L_SCALE;
+   printf("; %f, ", *y_axis);
+
+   // z_axis
+   lsb = magnet_buffer[3];
+   msb = (signed char) magnet_buffer[2];
+   tmp_axis = lsb + (msb << 8);
+   printf(", z: "BYTE_TO_BINARY_PATTERN" ", BYTE_TO_BINARY(magnet_buffer[2]));
+   printf(""BYTE_TO_BINARY_PATTERN" ", BYTE_TO_BINARY(magnet_buffer[3]));
+   *z_axis = ((signed int) tmp_axis) * HMC5883L_SCALE;
+   printf("; %f\n", *z_axis);
 
    return 0;
 }
